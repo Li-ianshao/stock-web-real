b4e578ffab6d5bacc79c6c8571e3cf9d5a035d97
Shao Li Yan <poi7415778@gmail.com>
2025-05-28 18:12:56 -0500

    建立個分頁templates之間的連結

--- core/urls.py
+++ core/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.tab_dividend_view, name='tab_dividend'),
+    path('dividend/', views.tab_dividend_view, name='tab_dividend'),
     path('rsi/', views.tab_rsi_view, name='tab_rsi'),
     path('bband/', views.tab_bband_view, name='tab_bband'),
     path('macd/', views.tab_macd_view, name='tab_macd'),
--- stockweb/urls.py
+++ stockweb/urls.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.shortcuts import redirect
 
 
 urlpatterns = [
+    path('', lambda request: redirect('tab_dividend')),
     path('accounts/', include('accounts.urls')),  # 登入功能專用路由
     path('admin/', admin.site.urls),
-    path('', include('core.urls')),
+    path('core/', include('core.urls')),
 ]
