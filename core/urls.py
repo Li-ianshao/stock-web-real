2ee32315ba3014af3999721aea577a1c7a13c380
Shao Li Yan <poi7415778@gmail.com>
2025-05-28 17:13:32 -0500

    建立templates中其他分頁與股票詳細頁面的檔案以及urls

--- core/urls.py
+++ core/urls.py
@@ -3,4 +3,9 @@ from . import views
 
 urlpatterns = [
     path('', views.tab_dividend_view, name='tab_dividend'),
+    path('rsi/', views.tab_rsi_view, name='tab_rsi'),
+    path('bband/', views.tab_bband_view, name='tab_bband'),
+    path('macd/', views.tab_macd_view, name='tab_macd'),
+    path('drop/', views.tab_drop_view, name='tab_drop'),
+    path('<str:symbol>/', views.stock_detail_view, name='stock_detail'),  # 詳細頁
 ]
