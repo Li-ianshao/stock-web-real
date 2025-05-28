30d29d607295418428cf42b172dbfc56228f5b79
Shao Li Yan <poi7415778@gmail.com>
2025-05-28 11:54:47 -0500

    階段二：建立登入頁面 login.html

--- core/views.py
+++ core/views.py
@@ -1,4 +1,6 @@
 from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required 
 
+@login_required
 def home(request):
-    return HttpResponse("Hello, world!")
+    return HttpResponse("🎉 登入成功！這是股票平台首頁")
