352869cb2168eb172396e4ac51c8755e2c1736b8
Shao Li Yan <poi7415778@gmail.com>
2025-05-28 20:45:43 -0500

    新增抓取S&P500股票資料與清除PICO暫存檔案功能

--- core/constants.py
+++ core/constants.py
@@ -1,4 +1,17 @@
+import pandas as pd
+
+# 從 Wikipedia 擷取 S&P 500 股票代碼
+SP500_SYMBOLS = None
+
 TEST_SYMBOLS = [
     'AAPL', 'MSFT', 'GOOGL', 'TSLA', 'JNJ',
     'KO', 'XOM', 'JPM', 'PG', 'NVDA', 'CCI', 'WU', 'TTE', 'BEN', 'CRWD', 'KSS'
 ]
+
+def load_sp500_symbols():
+    global SP500_SYMBOLS
+    if SP500_SYMBOLS is None:
+        url = "https://en.wikipedia.org/wiki/List_of_S%26P_500_companies"
+        df = pd.read_html(url)[0]
+        SP500_SYMBOLS = df['Symbol'].tolist()
+    return SP500_SYMBOLS
